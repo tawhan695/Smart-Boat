bab038944f617d4a9c92391d9c6dab23f01edec9
Tawhan Studio <36127485+tawhan695@users.noreply.github.com>
2019-06-07 20:09:11 +0700

    เพิ่มการอ่านข้อมูลจาก sensor
    อ่านค่าอนาล็อก แค่ตัวอย่าง

--- arduino-nano/code/code.ino
+++ arduino-nano/code/code.ino
@@ -1,9 +1,65 @@
-void setup() {
-  // put your setup code here, to run once:
+//nano
+#include <SoftwareSerial.h>
 
+SoftwareSerial sensor(5, 6); // RX, TX
+String g="";
+int DO =A0;
+int PH =A1;
+int TMP =A2;
+int EC =A3;
+int ORP =A4;
+float lnt =17.321266;
+float loa =104.108045;
+
+void setup() {
+  Serial.begin(57600);
+  while (!Serial) {
+    ; 
+  }
+
+
+  Serial.println("Goodnight moon!");
+  sensor.begin(19200);
+ // Gps.begin(19200);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { // run over and over
 
+                       //****************
+                    int IDo=analogRead(A0);
+                    int Do1=map(IDo,0,1023,1,14);
+   String Do=String(Do1);
+                    //****************
+                    int Iph=analogRead(A1);
+                    int pH=map(Iph,0,1023,1,14);
+   String Ph=String(pH);
+                     //****************
+                    int It=analogRead(A2);
+                    int Tmp=map(It,0,1023,1,14);
+  String Temp=String(Tmp);
+                     //****************
+                    int ec=analogRead(A3);
+                    int Ec=map(ec,0,1023,1,14);
+  String eC=String(Ec);
+                     //****************
+                       //****************
+                    int Orp=analogRead(A3);
+                    int ORp=map(Orp,0,1023,1,14);
+  String orp=String(ORp);
+    String data;
+    data +=Do;data +=",";
+    data +=Ph;data +=",";
+    data +=Temp;data +=",";
+    data +=eC;data +=",";
+    data +=orp;data +=",";
+    
+    g=sensor.readString();
+   Serial.print("data : ");
+   //Serial.println(GG);
+    Serial.println(data);
+    sensor.print(data);
+     delay(100);
+
+ 
 }
--- we-mos-mini/code/code.ino
+++ we-mos-mini/code/code.ino
@@ -1,22 +1,115 @@
--
+
 #define BLYNK_PRINT Serial
 
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <SoftwareSerial.h>
 
+SoftwareSerial chat(D5, D6); // RX, TX
 
 char ssid[] = "Tawhan-Studio";
 char pass[] = "T0655577688";
-char auth[] = "f0e9eb6e24124db1b7abeed355c46eda";
+char auth[] = "1c30bc20e4ab4a80ac9d80eaa040c23e";
+
+String a ="null";
+//17.321266, 104.108045
+float lat = 17.321266;
+float lon = 104.108045;
+
+//Status
+bool PH_sensor =false;
 
 void setup()
 {
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
+  Blynk.begin(auth, ssid, pass); 
+
+  chat.begin(19200);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 }
 
 void loop()
 {
   Blynk.run();
+  Map_GPS((lat),lon);
+  Data_nano();
+  
+Blynk.setProperty(V9, "url", "http://192.168.1.2:86/line.html");
+}
+
+void Data_nano(){
+     a = chat.readString();  //อ่าน Serial และนำไปเก็บในตัวแปร A
+   Serial.print("Data : "); 
+   Serial.println(a); 
+   pH("7");
+ chat.write("Do:10:ph:13:ec:50:tm:90");
+}
+WidgetMap myMap(V20);
+void Map_GPS(float lat,float lon){
+  
+  //MAP
+myMap.location(1, lat, lon, "ตำแหน่งเรือ");
+}
+void pH(String ph){
+  /*ค่า pH
+   คือค่าความเป็นเกรดเป็นเบส
+   โดยค่า pH มีอยู่ 1-14 
+   โดยค่ากลางคือ 7 
+   หากค่าที่วัดได้ต่ำกว่า 7 คือเกรด สูงกว่า 7 คือเบส
+  */
+  int pH=ph.toInt();
+  Blynk.virtualWrite(V30, pH);
+  if(pH==14){
+    Blynk.setProperty(V30, "color", "#92278F");
+  }else if(pH==13){
+     Blynk.setProperty(V30, "color", "#662E93");
+  }
+  else if(pH==12){
+    Blynk.setProperty(V30, "color", "#272264");
+  }
+  else if(pH==11){
+     
+     Blynk.setProperty(V30, "color", "#283890");
+  }
+  else if(pH==10){
+     Blynk.setProperty(V30, "color", "#0E76BD");
+    
+  }
+  else if(pH==9){
+    
+     Blynk.setProperty(V30, "color", "#25AAE3");
+  }
+  else if(pH==8){
+    
+      Blynk.setProperty(V30, "color", "#3AB54A");
+  }
+  else if(pH==7){
+     Blynk.setProperty(V30, "color", "#8DC73F");
+  }
+  else if(pH==6){
+      Blynk.setProperty(V30, "color", "#F3DA35");
+  }
+  else if(pH==5){
+    
+     Blynk.setProperty(V30, "color", "#FCB040");
+  }
+  else if(pH==4){
+
+     Blynk.setProperty(V30, "color", "#F7941D");
+  }
+  else if(pH==3){
+      Blynk.setProperty(V30, "color", "#F03F37");
+    
+  }
+  else if(pH==2){
+     Blynk.setProperty(V30, "color", "#F03F37");
+    
+  }
+  else if(pH==1){
+     Blynk.setProperty(V30, "color", "#BF1E2E");
+    
+  }
 }
